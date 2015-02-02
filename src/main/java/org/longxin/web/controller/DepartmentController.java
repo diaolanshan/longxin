07d269ad58c04ee276c56f7cfe0c39a8aba03414
diaolanshan <352350872@qq.com>
2015-02-02 18:59:43 +0800

    comments

--- src/main/java/org/longxin/web/controller/DepartmentController.java
+++ src/main/java/org/longxin/web/controller/DepartmentController.java
@@ -9,8 +9,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("department")
 public class DepartmentController
 {
-	@Autowired
-	DepartmentService departmentService;
+//	@Autowired
+//	DepartmentService departmentService;
 	
 	
 }
