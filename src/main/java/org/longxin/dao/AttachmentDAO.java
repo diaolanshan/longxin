1de8a60533ae8a2720a318b00cc75ce7020909d5
Eric Zhang <ericzhang8504@gmail.com>
2015-12-24 16:01:32 +0800

    test commit in eclipse

--- src/main/java/org/longxin/dao/AttachmentDAO.java
+++ src/main/java/org/longxin/dao/AttachmentDAO.java
@@ -4,6 +4,11 @@ import java.util.List;
 
 import org.longxin.domains.Attachment;
 
+/**
+ * Interface for the service which used to access the database object.
+ * @author ZER8SZH
+ *
+ */
 public interface AttachmentDAO {
 
 	public void saveOrUpdateAttachment(Attachment attachment);
